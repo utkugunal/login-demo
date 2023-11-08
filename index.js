2bfa92389dfbdca06c6e05e00c1b3ac10e98c0bb
utkugunal <145130606+utkugunal@users.noreply.github.com>
2023-11-08 17:36:03 +0100

    add section B and C for login

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const app = express();
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+app.get("/", (request, response) => {
+  console.log("Someone wants the root route!");
+  console.log("Welcome to our site! 😎");
+});
+
+app.get("/login", function (req, res) {
+  console.log("serving login.html...");
+  res.sendFile(path.join(__dirname, "/login.html"));
+});
